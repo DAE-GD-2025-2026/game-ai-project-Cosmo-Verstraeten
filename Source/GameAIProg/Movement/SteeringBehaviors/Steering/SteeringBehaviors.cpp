e4d1a67aeae85ea5ef13a4053d3fb69c9129b29c
Cosmo-Verstraeten <cosmo.verstraeten@student.howest.be>
2026-02-09 10:48:34 +0100

    Added Seek steeringbehavior

--- Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.cpp
+++ Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.cpp
@@ -4,3 +4,15 @@
 //SEEK
 //*******
 // TODO: Do the Week01 assignment :^)
+SteeringOutput Seek::CalculateSteering(float DeltaT, ASteeringAgent & Agent)
+{
+	SteeringOutput Steering{};
+	
+	Steering.LinearVelocity = Target.Position - Agent.GetPosition();.
+	//	linearvelocity is already normalized in steeringagent.cpp
+	
+	
+	// add debug rendering for grades 
+	
+	return Steering;
+}
